8317c4e1ec51c45c8c9a9d61410dc7a6229bded4
shivanipalanikumar <52229565+shivanipalanikumar@users.noreply.github.com>
2021-06-09 15:03:05 +0530

    Add files via upload

---
+++ orm-learn/src/main/java/com/cognizant/ormlearn/model/Country.java
@@ -0,0 +1,32 @@
+package com.cognizant.ormlearn.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="country")
+public class Country {
+	@Id
+	@Column(name="code")
+	private String countryCode;
+	@Column(name="name")
+	private String countryName;
+	public String getCountryCode() {
+		return countryCode;
+	}
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+	public String getCountryName() {
+		return countryName;
+	}
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+	@Override
+	public String toString() {
+		return "Country [countryCode=" + countryCode + ", countryName=" + countryName + "]";
+	}
+}
